2978931c8710f30b15589e3bf844f13fac879cce
tomaspc <mil-tomas@seznam.cz>
2025-06-15 12:12:12 +0200

    feat: dynamic highlighting

--- src/components/AsmDiffEditor.tsx
+++ src/components/AsmDiffEditor.tsx
@@ -1,22 +1,24 @@
-import { diff as DiffEditor } from 'react-ace';
+import { diff as DiffEditor } from "react-ace";
 import "ace-builds/src-noconflict/mode-assembly_x86";
 import "ace-builds/src-noconflict/theme-kuroir";
 import "ace-builds/src-noconflict/theme-monokai";
 import "./Diff.css";
-import { useColorModeValue } from './ui/color-mode';
+import { useColorModeValue } from "./ui/color-mode";
 
 export default function AsmDiffView() {
-
-	const theme = useColorModeValue("kuroir", "monokai");
+	const editorTheme = useColorModeValue("kuroir", "monokai");
+	const theme = useColorModeValue("light", "dark");
 
 	return (
-		<DiffEditor 
-			value={["Ahoj\nnashle", "nashle"]}
-			theme={theme}
-			mode="assembly_x86"
-			focus={false}
-			showGutter={true}
-			readOnly={true}
-		/>
+		<div className={theme}>
+			<DiffEditor
+				value={["Ahoj\nnashle", "nashle"]}
+				theme={editorTheme}
+				mode="assembly_x86"
+				focus={false}
+				showGutter={true}
+				readOnly={true}
+			/>
+		</div>
 	);
 }
