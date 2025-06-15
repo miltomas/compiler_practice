02bb0be889b0d31a5dbf7ee41a6ebc9765067aa5
tomaspc <mil-tomas@seznam.cz>
2025-06-15 16:26:04 +0200

    feat: use localstorage to store preferences

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,17 @@ import { Route, Routes } from "react-router";
 import "./App.css";
 import HomePage from "./pages/HomePage";
 import Header from "./components/Header";
+import { useContext, useEffect } from "react";
+import { EditorContext, type EditorBinds } from "./contexts/EditorContext";
 
 function App() {
+	const editorCtx = useContext(EditorContext);
+
+	useEffect(() => {
+		editorCtx.setBindings(localStorage.getItem("binds") as EditorBinds ?? "");
+		editorCtx.setScale(Number(localStorage.getItem("scale")) ?? 14);
+	}, []);
+
 	return (
 		<>
 		<Header />
--- src/components/Settings/Settings.tsx
+++ src/components/Settings/Settings.tsx
@@ -1,33 +1,40 @@
 import { Button, CloseButton, Drawer, Portal } from "@chakra-ui/react";
 import { LuSettings } from "react-icons/lu";
 import SettingsContent from "./SettingsContent";
+import { useContext } from "react";
+import { EditorContext } from "@/contexts/EditorContext";
 
 export default function Settings() {
-	return (
-		<Drawer.Root size={{ base: "full", md: "lg" }}>
-			<Drawer.Trigger asChild>
-				<Button variant="ghost">
-					<LuSettings />
-				</Button>
-			</Drawer.Trigger>
-			<Portal>
-				<Drawer.Backdrop />
-				<Drawer.Positioner>
-					<Drawer.Content>
-						<Drawer.Header>
-							<Drawer.Title>Settings</Drawer.Title>
-						</Drawer.Header>
-						<Drawer.Body>
-							<SettingsContent />
-						</Drawer.Body>
-						<Drawer.CloseTrigger asChild>
-							<CloseButton size="sm" />
-						</Drawer.CloseTrigger>
-					</Drawer.Content>
-				</Drawer.Positioner>
-			</Portal>
-		</Drawer.Root>
-	);
+  const editorCtx = useContext(EditorContext);
+  return (
+    <Drawer.Root
+      size={{ base: "full", md: "lg" }}
+      onExitComplete={() => {
+        localStorage.setItem("binds", editorCtx.bindings);
+        localStorage.setItem("scale", editorCtx.scale.toString());
+      }}
+    >
+      <Drawer.Trigger asChild>
+        <Button variant="ghost">
+          <LuSettings />
+        </Button>
+      </Drawer.Trigger>
+      <Portal>
+        <Drawer.Backdrop />
+        <Drawer.Positioner>
+          <Drawer.Content>
+            <Drawer.Header>
+              <Drawer.Title>Settings</Drawer.Title>
+            </Drawer.Header>
+            <Drawer.Body>
+              <SettingsContent />
+            </Drawer.Body>
+            <Drawer.CloseTrigger asChild>
+              <CloseButton size="sm" />
+            </Drawer.CloseTrigger>
+          </Drawer.Content>
+        </Drawer.Positioner>
+      </Portal>
+    </Drawer.Root>
+  );
 }
-
-
