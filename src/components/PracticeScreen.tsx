4da4a02be09b143af011313ffac9de809ff1e39b
tomaspc <mil-tomas@seznam.cz>
2025-06-16 16:43:30 +0200

    feat: add function to post to godbolt

--- src/components/PracticeScreen.tsx
+++ src/components/PracticeScreen.tsx
@@ -3,40 +3,81 @@ import Editor from "./editors/Editor";
 import { useRef } from "react";
 
 export type PracticeScreenProps = {
-	onUpload: () => void;
+  onUpload: () => void;
 };
 
+async function postToGodbolt(
+  compiler: string,
+  payload: { source: string; options: string },
+) {
+  const url = `https://godbolt.org/api/compiler/${compiler}/compile`;
+
+  try {
+    const response = await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(payload),
+    });
+
+    if (!response.ok) {
+      throw new Error(`HTTP error ${response.status}: ${response.statusText}`);
+    }
+
+    const data = await response.text();
+    console.log("Response JSON:", data);
+
+    return data;
+  } catch (err) {
+    console.error("Error during fetch:", err);
+    throw err;
+  }
+}
+
 export default function PracticeScreen() {
+  const highLevelCode = useRef("");
+  const predictedCode = useRef("");
 
-	const highLevelCode = useRef("");
-	const predictedCode = useRef("");
-
-	return (
-		<Stack w="100%" h="100%" align="center" p="10px">
-			<Flex flexDirection={{ base: "column" , md: "row" }} w="100%" h="100%" gap="10px">
-				<Stack w="100%" h="100%">
-					<Heading>Practice code</Heading>
-					<Editor
-						onChange={(value) => highLevelCode.current = value}
-						mode="c_cpp"
-						name="c"
-						key="c"
-					/>
-				</Stack>
-				<Stack w="100%" h="100%">
-					<Heading>Assembly</Heading>
-					<Editor
-						onChange={(value) => predictedCode.current = value}
-						mode="assembly_x86"
-						name="asm"
-						key="asm"
-					/>
-				</Stack>
-			</Flex>
-			<Button w={{ base: "100%", md: "40%" }}
-			/*onClick={() => fetch()}*/>
-				Upload
-			</Button>
-		</Stack>
-	);
+  return (
+    <Stack w="100%" h="100%" align="center" p="10px">
+      <Flex
+        flexDirection={{ base: "column", md: "row" }}
+        w="100%"
+        h="100%"
+        gap="10px"
+      >
+        <Stack w="100%" h="100%">
+          <Heading>Practice code</Heading>
+          <Editor
+            onChange={(value) => (highLevelCode.current = value)}
+            mode="c_cpp"
+            name="c"
+            key="c"
+          />
+        </Stack>
+        <Stack w="100%" h="100%">
+          <Heading>Assembly</Heading>
+          <Editor
+            onChange={(value) => (predictedCode.current = value)}
+            mode="assembly_x86"
+            name="asm"
+            key="asm"
+          />
+        </Stack>
+      </Flex>
+      <Button
+        w={{ base: "100%", md: "40%" }}
+        onClick={async () => {
+          const asm = await postToGodbolt("g95", {
+            source: '#include <cstdio>\n int main() { printf("ahoj"); }',
+            options: "",
+          });
+          console.log(asm);
+        }}
+      >
+        Upload
+      </Button>
+    </Stack>
+  );
 }
