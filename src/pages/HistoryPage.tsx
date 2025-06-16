59b9f89c4224726837d3b62a93aada6af68dc8c2
tomaspc <mil-tomas@seznam.cz>
2025-06-16 22:19:58 +0200

    feat: add history page

--- src/pages/HistoryPage.tsx
+++ src/pages/HistoryPage.tsx
@@ -1,15 +1,41 @@
 import { PracticeContext, type Practice } from "@/contexts/PracticeContext";
-import { Heading } from "@chakra-ui/react";
+import { Card, EmptyState, Heading, HStack, VStack } from "@chakra-ui/react";
 import { useContext } from "react";
 
 export default function HistoryPage() {
   const { history } = useContext(PracticeContext);
 
   return (
-    <div>
-      {history.map((item: Practice, index: number) => (
-        <Heading key={index}>skibidi</Heading>
-      ))}
-    </div>
+    <>
+      {history.length > 0 ? (
+        history.map((item: Practice, index: number) => (
+          <Card.Root size="md">
+            <Card.Header>
+              <Heading size="md">Practice {index}</Heading>
+            </Card.Header>
+            <Card.Body color="fg.muted">
+				<HStack gap="80px">
+				<span>
+					{"Adds: " + item.changes.adds.toString()}
+				</span>
+				<span>
+					{"Removes: " + item.changes.removes.toString()}
+				</span>
+				</HStack>
+            </Card.Body>
+          </Card.Root>
+        ))
+      ) : (
+        <EmptyState.Root size="lg">
+          <EmptyState.Content>
+            <EmptyState.Indicator></EmptyState.Indicator>
+            <VStack textAlign="center">
+              <EmptyState.Title>Your history is empty</EmptyState.Title>
+              <EmptyState.Description>Do some work</EmptyState.Description>
+            </VStack>
+          </EmptyState.Content>
+        </EmptyState.Root>
+      )}
+    </>
   );
 }
