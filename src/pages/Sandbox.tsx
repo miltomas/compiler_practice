ec4d1f92350907115f80dee1b4d730c3777d0f0c
tomasntb <mil-tomas@seznam.cz>
2025-06-16 12:54:41 +0200

    feat: make layout responsive

--- src/pages/Sandbox.tsx
+++ src/pages/Sandbox.tsx
@@ -1,9 +1,9 @@
 import Editor from "@/components/editors/Editor";
-import { Heading, HStack, Stack } from "@chakra-ui/react";
+import { Flex, Heading, Stack } from "@chakra-ui/react";
 
 export default function Sandbox() {
 	return (
-		<HStack w="100%" h="100%" p="20px" gap="20px">
+		<Flex flexDirection={{ base: "column" , md: "row" }} w="100%" h="100%" p="20px" gap="20px">
 			<Stack w="100%" h="100%">
 				<Heading>C code</Heading>
 				<Editor
@@ -22,6 +22,6 @@ export default function Sandbox() {
 					key="asm"
 				/>
 			</Stack>
-		</HStack>
+		</Flex>
 	);
 }
