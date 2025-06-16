7908e14ef633ba3adf465599b45ab32bfc3b149a
tomaspc <mil-tomas@seznam.cz>
2025-06-16 22:56:16 +0200

    fix: use useEffect

--- src/pages/Sandbox.tsx
+++ src/pages/Sandbox.tsx
@@ -1,16 +1,18 @@
 import PracticeScreen from "@/components/PracticeScreen";
 import { PracticeContext, type Practice } from "@/contexts/PracticeContext";
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
 
 export default function Sandbox() {
   const { history, addPractice } = useContext(PracticeContext);
 
   const onSubmitEventHandler = (value: Practice) => {
-	  addPractice(value);
-	  localStorage.setItem("history", JSON.stringify(history));
+    addPractice(value);
   };
 
-  return (
-    <PracticeScreen onSubmit={onSubmitEventHandler}/>
+  useEffect(
+    () => localStorage.setItem("history", JSON.stringify(history)),
+    [history],
   );
+
+  return <PracticeScreen onSubmit={onSubmitEventHandler} />;
 }
